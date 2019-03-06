d4acb3a257ecf6eb242515906ecd8a8434016ba2
Vaishali Kushwaha <vaishalikushwaha@vaishalis-mbp.wkstn.qmul.ac.uk>
2019-03-06 20:36:52 +0000

    details/clothing button

--- weather-app/src/components/clothing/index.js
+++ weather-app/src/components/clothing/index.js
@@ -1,7 +1,7 @@
 // import preact
 import { h, render, Component } from 'preact';
 
-export default class Details extends Component {
+export default class Clothing extends Component {
 
 	// rendering a function when the button is clicked
 	render() {
@@ -14,7 +14,7 @@ export default class Details extends Component {
 		return (
 			<div>
 				<button onClick={cFunction}>
-					<p style = "font-size: 9px; text-align: center; width: 170px;">DETAILS</p>
+					<p style = "font-size: 9px; text-align: center; width: 170px;">CLOTHING</p>
 				</button>
 			</div>
 		);
